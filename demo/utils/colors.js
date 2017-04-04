aeda7fe209e95616f5f98eab927d3851a9570f82
Phil Plueckthun <phil@plckthn.me>
2017-04-05 00:47:05 +0100

    Remove polished from bundle

--- demo/utils/colors.js
+++ demo/utils/colors.js
@@ -5,4 +5,4 @@ export const foreground = '#f8f8f2'
 export const red = '#ff5555'
 
 export const blue = polished.lighten(0.1, '#6272a4')
-export const lightGrey = polished.darken(0.05, background)
+export const lightGrey = polished.darken(0.05, '#282a36')
