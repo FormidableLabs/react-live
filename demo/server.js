1b6f95f22b21ed5af0cd2a8a751a539fddbc797b
Phil Plueckthun <phil@plckthn.me>
2017-04-10 12:35:18 +0100

    Update react-live demo page

--- demo/server.js
+++ demo/server.js
@@ -6,21 +6,62 @@ if (!dev) {
   moduleAlias.addAlias('react-dom', 'preact-compat')
 }
 
-const { createServer } = require('http')
 const { parse } = require('url')
+const express = require('express')
+const LRUCache = require('lru-cache')
 const next = require('next')
 
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-.then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  })
-  .listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 * 24 // 24h
 })
+
+const cachedRender = (req, res, pagePath, queryParams) => {
+  const key = `${req.url}`
+
+  if (!dev && ssrCache.has(key)) {
+    res.append('X-Cache', 'HIT')
+    res.send(ssrCache.get(key))
+    return
+  }
+
+  app.renderToHTML(req, res, pagePath, queryParams)
+    .then(html => {
+      ssrCache.set(key, html)
+
+      res.append('X-Cache', 'MISS')
+      res.send(html)
+    })
+    .catch((err) => {
+      app.renderError(err, req, res, pagePath, queryParams)
+    })
+}
+
+const PORT = process.env.PORT || 3000
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    server.disable('x-powered-by')
+
+    server.get('/', (req, res) => {
+      cachedRender(req, res, '/')
+    })
+
+    server.get('*', (req, res) => {
+      const parsedUrl = parse(req.url, true)
+      handle(req, res, parsedUrl)
+    })
+
+    server.listen(PORT, err => {
+      if (err) {
+        throw err
+      }
+
+      console.log(`> Ready on http://localhost:${PORT}`)
+    })
+  })
