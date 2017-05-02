88d5df8a1f4bf74c884e5d61a5c74b2421b3d3e0
Phil Plückthun <phil@plckthn.me>
2017-05-03 00:43:39 +0100

    Fix newline throw for empty indentation

--- src/utils/getIndent.js
+++ src/utils/getIndent.js
@@ -4,9 +4,12 @@ const getIndent = (plain, cursorPos) => {
   const startSlice = plain.slice(0, cursorPos)
   const lastNewline = startSlice.lastIndexOf('\n') + 1
   const lineSlice = startSlice.slice(lastNewline)
-  const indentation = lineSlice.match(indentRegex)[0]
+  const matches = lineSlice.match(indentRegex)
+  if (matches === null) {
+    return ''
+  }
 
-  return indentation || ''
+  return matches[0] || ''
 }
 
 export default getIndent
