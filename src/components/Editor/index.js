766d2d698f5deac1895dfc083b4a7796d25bf0ef
David Burgess <i.am@dburg.es>
2017-07-17 23:35:28 -0400

    tabGuarded will no longer be set on blur to avoid outline showing on elements before you change focus to another. This however introduces behavior where clicking out of an Editor without a tabGuard and then tab navigating to it will capture tab until esc is pressed.  Not ideal but better than the previous behavior of the jumping outline.

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -168,14 +168,6 @@ class Editor extends Component {
     }
   }
 
-  onBlur = evt =>{
-    if (this.props.onBlur) {
-      this.props.onBlur(evt)
-    }
-    if(!this.state.tabGuarded){
-      this.setState({tabGuarded:true})
-    }
-  }
   onClick = evt => {
     if (this.props.onClick) {
       this.props.onClick(evt)
@@ -224,7 +216,6 @@ class Editor extends Component {
         onKeyDown={contentEditable && this.onKeyDown}
         onKeyUp={contentEditable && this.onKeyUp}
         onClick={contentEditable && this.onClick}
-        onBlur={contentEditable && this.onBlur}
         dangerouslySetInnerHTML={{ __html: html }}
       />
     )
