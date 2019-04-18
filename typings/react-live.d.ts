7eab501b99e32faa4ba21e3135f03426bb76bb9e
hasparus <hasparus@gmail.com>
2019-04-18 10:21:20 +0200

    Use prism-react-renderer types

--- typings/react-live.d.ts
+++ typings/react-live.d.ts
@@ -1,4 +1,5 @@
-import { ComponentClass, StatelessComponent, HTMLProps, ComponentType, CSSProperties } from 'react'
+import { ComponentClass, StatelessComponent, HTMLProps, ComponentType } from 'react'
+import { PrismTheme, Language } from 'prism-react-renderer';
 
 // Helper types
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
@@ -13,12 +14,9 @@ export type LiveProviderProps = Omit<DivProps, 'scope'> & {
   code?: string;
   noInline?: boolean;
   transformCode?: (code: string) => string;
-  language?: string;
+  language?: Language;
   disabled?: boolean;
-  theme?: {
-    plain: CSSProperties;
-    styles: Array<{ types: string[]; styles: CSSProperties }>;
-  }
+  theme?: PrismTheme;
 }
 
 export const LiveProvider: ComponentClass<LiveProviderProps>
