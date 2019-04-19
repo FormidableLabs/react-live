ce9bc2b733fdec75c487b735bf1c261e93138b7b
Ben Mosher <me@benmosher.com>
2019-04-19 07:23:15 -0400

    expose `theme` prop
    added `type Theme = any`, not sure exactly what the type should be.

--- typings/react-live.d.ts
+++ typings/react-live.d.ts
@@ -7,12 +7,15 @@ type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 type DivProps = HTMLProps<HTMLDivElement>
 type PreProps = HTMLProps<HTMLPreElement>
 
+type Theme = any
+
 // LiveProvider
 export type LiveProviderProps = Omit<DivProps, 'scope'> & {
   scope?: { [key: string]: any };
   code?: string;
   noInline?: boolean;
   transformCode?: (code: string) => string;
+  theme?: Theme;
 }
 
 export const LiveProvider: ComponentClass<LiveProviderProps>
@@ -26,7 +29,8 @@ export const Editor: ComponentClass<EditorProps>
 
 // LiveEditor
 export type LiveEditorProps = Omit<EditorProps, 'onChange'> & {
-  onChange?: (code: string) => void
+  onChange?: (code: string) => void;
+  theme?: Theme;
 }
 
 export const LiveEditor: ComponentClass<LiveEditorProps>
