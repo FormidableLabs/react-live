b9385d92bc53cce704e285d9a9d6b2dcb3131a87
JP <jpdriver@users.noreply.github.com>
2021-06-27 07:23:41 -0700

    fixup webpack resolution for demo site

--- demo/next.config.js
+++ demo/next.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = {
   webpack: config => {
     config.resolve.alias['react-live'] = path.resolve('../');
+    config.resolve.alias.react = path.resolve('./node_modules/react');
+    config.resolve.alias['react-dom'] = path.resolve('./node_modules/react-dom');
 
     return config;
   }
