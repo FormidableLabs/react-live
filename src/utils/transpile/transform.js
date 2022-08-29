846194de381209f3dc7a76e1fa27730726dcb22e
Guo Yunhe <i@guoyunhe.me>
2022-08-29 10:42:54 +0800

    Support typescript transform

--- src/utils/transpile/transform.js
+++ src/utils/transpile/transform.js
@@ -1,5 +1,5 @@
 import { transform as _transform } from "sucrase";
 
-const opts = { transforms: ["jsx", "imports"] };
+const opts = { transforms: ["typescript", "jsx", "imports"] };
 
 export default (code) => _transform(code, opts).code;
