1594759856a1a72d2617e29ca1ababf487f4f1d6
Carlos Kelly <me@carlos.dev>
2023-03-25 10:06:51 -0500

    Add additional copy to explain demos

--- website/src/components/live-edit.tsx
+++ website/src/components/live-edit.tsx
@@ -4,7 +4,7 @@ import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 export const DemoLiveEditor = ({ noInline, code }) => {
   return (
     <LiveProvider code={code} noInline={noInline}>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid lg:grid-cols-2 gap-4">
         <LiveEditor className="font-mono" />
         <LivePreview />
       </div>
