92ffd7a89ece5ac0204f3956d1029e4471bed3f4
Carlos Kelly <me@carlos.dev>
2023-03-30 12:28:23 -0500

    Set up Formideploy for docs

---
+++ website/formideploy.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  lander: {
+    name: "react-live",
+  },
+  build: {
+    // Build output directory.
+    // Docusaurus defaults to `build`.
+    // We use `docusaurus build --out-dir build/open-source/docusaurus` to build intermediate real dirs.
+    dir: "build",
+  },
+};
